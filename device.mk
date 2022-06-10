c0b8ab6066f709f5f172039eb8f815addd31f291
Ichigokano <3323946706@qq.com>
2022-06-10 10:53:44 +0000

    cannon:drop libandroidicu

--- device.mk
+++ device.mk
@@ -19,7 +19,6 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster41 \
     libkeymaster4 \
     libpuresoftkeymasterdevice \
-    libandroidicu \
     libion
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
