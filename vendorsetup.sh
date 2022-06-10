39f3284dac0bd31f0c3607ae09d904c4b2a0f1f5
Ichigokano <3323946706@qq.com>
2022-06-10 10:35:38 +0000

    change omni to twrp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_cannon-user
-add_lunch_combo omni_cannon-userdebug
-add_lunch_combo omni_cannon-eng
+#add_lunch_combo omni_cannon-user
+#add_lunch_combo omni_cannon-userdebug
+#add_lunch_combo omni_cannon-eng
