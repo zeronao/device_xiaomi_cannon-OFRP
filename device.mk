515ea6f03588d3217dc7e93d8b223c4374b422c5
Ichigokano <3323946706@qq.com>
2022-06-10 10:47:06 +0000

    cannon:correct fastbootd

--- device.mk
+++ device.mk
@@ -7,6 +7,15 @@
 
 LOCAL_PATH := device/xiaomi/cannon
 
+# Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
+
+# Additional target Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hardware.keymaster@4.1 \
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
