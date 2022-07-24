3b89a6f071b07001deed6141535d36513c17c905
AngelaCooljx <ljx15007902367@gmail.com>
2022-07-24 08:24:18 +0800

    Adapt to cannon

--- device.mk
+++ device.mk
@@ -7,49 +7,14 @@
 
 LOCAL_PATH := device/xiaomi/cannon
 
-# Partitions
+# Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 # Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
-    fastbootd 
 TW_INCLUDE_FASTBOOTD := true
 
 # API
 PRODUCT_TARGET_VNDK_VERSION := 31
 PRODUCT_SHIPPING_API_LEVEL := 29
-
-# Drm
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4
-
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-
-# Keymint
-PRODUCT_PACKAGES += \
-    android.hardware.security.keymint \
-    android.hardware.security.secureclock \
-    android.hardware.security.sharedsecret
-
-# Keymint
-PRODUCT_PACKAGES += \
-    android.hardware.security.keymint \
-    android.hardware.security.secureclock \
-    android.hardware.security.sharedsecret
-    
-# Keystore
-PRODUCT_PACKAGES += \
-    android.system.keystore2
-    
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1 \
-	android.hardware.keymaster@4.0
 
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
@@ -61,6 +26,3 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-
-#config
-TW_FRAMERATE := 60
