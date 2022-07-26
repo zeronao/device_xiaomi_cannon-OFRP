19d9c2d9bd0f97c8395bbf3e9c530c58e4071aa9
Uzeda <gabrielsuzeda@gmail.com>
2022-07-25 21:01:17 -0300

    updated kernel and cleanup in vendorsetup
    kernel changed for better performance and possible fix for miui ota

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# 关于橙狐变量详见 OrangeFox_10/fox_10.0/vendor/recovery/orangefox_build_vars.txt
+FDEVICE="cannon"
+DEVICE_PATH="device/xiaomi/cannon"
 
 	export LC_ALL="C"
 	export ALLOW_MISSING_DEPENDENCIES=true
@@ -19,8 +20,6 @@
 	## 添加功能
 	# 使用完整版ps命令
 	export FOX_REPLACE_BUSYBOX_PS=1
-	# 使用完整版getprop命令
-	export FOX_REPLACE_TOOLBOX_GETPROP=1
 	# 支持tar命令
 	export FOX_USE_TAR_BINARY=1
 	# 支持sed命令
@@ -53,17 +52,13 @@
 	# 由于橙狐默认屏幕比例是16：9,所以应设置屏幕高度为屏幕比例换算成n：9之后，n*120
 	export OF_SCREEN_H=2340
 	export OF_STATUS_H=120
-	export OF_STATUS_INDENT_LEFT=120
-	export OF_STATUS_INDENT_RIGHT=90
+	export OF_STATUS_INDENT_LEFT=160
+	export OF_STATUS_INDENT_RIGHT=20
 	# 添加黑色状态栏（隐藏刘海）选项
 	export OF_HIDE_NOTCH=1
 	# 由于有刘海遮挡，设置时钟位置为只能显示在左侧或右侧
 	export OF_CLOCK_POS=1
-	# 禁止禁用导航栏
-	export OF_ALLOW_DISABLE_NAVBAR=0
 
-	## 使刷机包兼容红米note9 5G
-	export TARGET_DEVICE_ALT="cannon,cannong"
 
 	## 添加橙狐特殊处理
 	# 设定recovery、system、vendor、boot分区路径
@@ -78,24 +73,23 @@
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 	# 跳过FBE解密流程（防止卡在橙狐LOGO或Redmi/Mi LOGO界面）
 	# export OF_SKIP_FBE_DECRYPTION=1
-	# 当ROM大于等于指定安卓SDK等级时，跳过FBE解密流程
-	# export OF_SKIP_FBE_DECRYPTION_SDKVERSION=31
 	# 在MIUI OTA还原期间尝试解密内部存储（而不是错误退出）
-	export OF_OTA_RES_DECRYPT=1
+ 	export F_NO_MIUI_OTA_VENDOR_BACKUP=1
+	export OF_FORCE_DISABLE_DM_VERITY=1
+	export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
+	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 	# 防止橙狐在解密后重新运行自启动进程
 	export OF_NO_RELOAD_AFTER_DECRYPTION=1
 	# 设置一个很老的build时间，用于解决某些ROM例如MIUI刷机脚本里的防回滚保护检测
 	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
 	# 防止用户在不使用metadata加密的ROM中重复被metadata装载错误提示刷屏
 	export OF_FBE_METADATA_MOUNT_IGNORE=1
-	# 手动刷入OTA增量包时尝试进入OpenRecoveryScript模式完成更新
-	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+
 	# 尝试处理AVB2.0，防止橙狐被官方recovery替换，实测此机型无效
-	# export OF_PATCH_AVB20=1
+	export OF_PATCH_AVB20=1
 	# 关闭修改橙狐启动画面功能，这个功能可能会导致重启无限卡MI
 	export OF_NO_SPLASH_CHANGE=1
-	# 禁用橙狐内置的magisk菜单
-	export FOX_DELETE_MAGISK_ADDON=0
 	# 使用指定的magisk
         export FOX_USE_SPECIFIC_MAGISK_ZIP="$PWD/device/xiaomi/cannon/recovery/root/FFiles/Magisk.zip"
 	# 使用指定的magisk版本号，由于magisk 23+使用了新的包装形式，文件路径改变了，橙狐无法获取正确的版本
@@ -103,8 +97,10 @@
 	# 当修补recovery/boot镜像时，始终指示magiskboot v24+修补vbmeta标头（自动禁用avb验证？）
 	export OF_USE_MAGISKBOOT=1
 	export OF_PATCH_VBMETA_FLAG=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 	# 修复部分设备或者rom的解密问题
 	export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
+	export OF_OTA_RES_DECRYPT=1
+      
+
 
 	echo -e "\x1b[96mcannon: 当你看到这个消息的时候，所有的OrangeFox Var已经添加完毕！\x1b[m"
